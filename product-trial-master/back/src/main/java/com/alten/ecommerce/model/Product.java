9ac78caa45350e60ba6d338ed82bd70b457cb879
Wael Bensoltana <wael.bensoltana@gmail.com>
2025-06-23 19:43:29 +0200

    Configuration & Product

---
+++ product-trial-master/back/src/main/java/com/alten/ecommerce/model/Product.java
@@ -0,0 +1,34 @@
+package com.alten.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+    private String image;
+    private String category;
+    private double price;
+    private int quantity;
+    private String internalReference;
+    private Long shellId;
+
+    @Enumerated(EnumType.STRING)
+    private InventoryStatus inventoryStatus;
+    private double rating;
+    private Long createdAt;
+    private Long updatedAt;
+
+    public enum InventoryStatus {
+        INSTOCK, LOWSTOCK, OUTOFSTOCK
+    }
+}
