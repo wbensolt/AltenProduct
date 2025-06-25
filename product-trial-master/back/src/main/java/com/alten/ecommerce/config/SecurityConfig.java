c4638c183edfd91ce5a47d9bffc3d809532eaa7c
Wael Bensoltana <wael.bensoltana@gmail.com>
2025-06-25 11:36:34 +0200

    Security Acess

--- product-trial-master/back/src/main/java/com/alten/ecommerce/config/SecurityConfig.java
+++ product-trial-master/back/src/main/java/com/alten/ecommerce/config/SecurityConfig.java
@@ -1,32 +1,32 @@
 package com.alten.ecommerce.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtFilter jwtFilter;
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf().disable()
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf
+                .ignoringRequestMatchers("/h2-console/**") // ignore CSRF pour H2
+            )
+            .headers(headers -> headers
+                .frameOptions(frameOptions -> frameOptions.sameOrigin()) // autorise les frames pour H2
+            )
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/token", "/account").permitAll()
+                .requestMatchers("/account", "/token", "/h2-console/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+            .httpBasic(Customizer.withDefaults()); // si tu veux tester en mode basique
+
+        return http.build();
     }
 }
-
